37255d8ef5e87bfaa079cd1064db4ca9e999474e
blitzher <skovbooorg@gmail.com>
2019-11-27 01:14:20 +0100

    added cool ascii

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-import pygame as pg
-import math
-
+import pygame as pg, numpy as np
+import pygame
 sh, sw = (500, 500)
 white = (255, 255, 255)
 black = (0, 0, 0)
-
+pygame.draw.line
 screen = pg.display.set_mode((sh, sw))
 running = True
+           
+#                   ..      .........   .u*"^" "^Rc         
+#                 oP""*Lo*#"""""""""""7d" .d*N.    $         
+#                @  u@""           .u*" o*"   #L    ?b        
+#              8  ^"                    £w*@me€      '"Nu    
+#           .P                                          $r 
+#         .@"                                  $L       $  
+#    .d#"                                  .dP d"   .d#     
+#   xP              .e                 .ud#"  dE.o@"(       
+#   $             s*"              .u@*""     '""\dP"       
+#   ?L  ..                    ..o@""        .$  uP          
+#    #c:$"*u.             .u@*""$          uR .@"           
+#     ?L$. '"""***Nc    x@""   @"         d" JP             
+#       @'          "b.'$.   @"         $" 8"               
+#                    @L    $"         d" 8\                 
+#                    $$u.u$"         dF dF      tony        
+#                    $ """   o      dP xR                   
+#                    $      dFNu...@"  $                    
+#                    "N..   ?B ^"""   :R                    
+#                      """"* RL       d>                    
+#                              ^"*bo@"                     
+
 
 pg.init()
 
+def normalize(array):
+    vector_length = np.linalg.norm(array)
+    return array / vector_length
+1
 def axis(screen):
     pg.draw.line(screen, black, (0, sh/2), (sw, sh/2))
     pg.draw.line(screen, black, (sw/2, 0), (sw/2, sh))
@@ -18,16 +43,25 @@ def axis(screen):
 " y' = x + y "
 
 def dfun(x,y):
-    return (x + y)*1/500
+    return (y)*5/500
 
-def field(screen, fun):
+
+def draw_func(screen, fun):
+    pass
+
+def draw_field(screen, fun):
     n = 20
-    for x in range(-250, 250, int(sw/n)):
-        for y in range(-250, 250, int(sh/n)):
-            slope = dfun(x,y) * 500/n
+    for x in range(0, sw, int(sw/n)):
+        for y in range(0, sh, int(sh/n)):
 
-            end_point = int(x + 250 + 250/n), int(-y + 250 - slope)
-            pg.draw.aaline(screen, black, (int(x+250),int(-y+250)), end_point)
+            vector_length = max(sw, sh)/(2*n)
+
+            slope_vector = normalize((1, -fun(x-sw/2,-y+sw/2) ))
+
+            start_point = np.array((x, y))  - slope_vector * vector_length / 2
+            end_point =   np.array((x, y))  + slope_vector * vector_length / 2
+
+            pg.draw.aaline(screen, black, start_point, end_point, 2)
 
 
 while running:
@@ -40,7 +74,7 @@ while running:
             running = False
 
     axis(screen)
-    field(screen, dfun)
+    draw_field(screen, dfun)
 
     pg.display.flip()
     pass
